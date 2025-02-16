5a48ebd38678d7af7b891595c6ff8740bcf79a0a
Andrew Beketov <andrewbeketov03@tamu.edu>
2025-02-16 00:29:38 -0600

    add date range picker

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -1,4 +1,8 @@
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MdOutlineDateRange } from "react-icons/md";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 
 export default function DatePicker({ date, setDate }) {
     return (
@@ -7,13 +11,29 @@ export default function DatePicker({ date, setDate }) {
                 <MdOutlineDateRange className="text-[24px]" />
                 <p className="font-[Alexandria] font-medium text-[20px] leading-[24.38px] text-gray-800">Date</p>
             </div>
-            <input
-                type="text"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                placeholder="choose your start and end dates"
-                className="mt-1 w-full p-2 text-gray-500 text-sm border-b border-gray-200 focus:outline-none focus:border-gray-400"
-            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DateRangePicker']}>
+                    <DateRangePicker
+                        value={date}
+                        onChange={(newValue) => setDate(newValue)}
+                        localeText={{ start: 'Check-in', end: 'Check-out' }}
+                        className="mt-1 w-full"
+                        slotProps={{
+                            textField: {
+                                variant: "standard",
+                                sx: {
+                                    '& .MuiInput-underline:before': {
+                                        borderBottomColor: '#E5E7EB'
+                                    },
+                                    '& .MuiInput-underline:hover:before': {
+                                        borderBottomColor: '#9CA3AF'
+                                    }
+                                }
+                            }
+                        }}
+                    />
+                </DemoContainer>
+            </LocalizationProvider>
         </div>
     );
 }

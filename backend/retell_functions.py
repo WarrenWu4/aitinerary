99676b6f3d3a80ba1e68b0b74a093fce0d24eafc
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2025-02-16 09:57:09 -0600

    Removed some auth checks

---
+++ backend/retell_functions.py
@@ -0,0 +1,24 @@
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+from functools import wraps
+from flask import Flask, redirect, render_template, session, url_for, jsonify
+import os
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+from models import get_user_by_oauth_id
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
+print(app.secret_key)
+
+retell_key = os.getenv("RETELL_KEY")
+
+from retell import Retell
+
+retell_client = Retell(
+    api_key="YOUR_API_KEY"
+)
